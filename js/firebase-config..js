9b8e82ee680aa9c47377a4f366369d7440980e9a
gambito82-crypto <gambito82@gmail.com>
2026-04-24 16:06:59 -0400

    Add files via upload

---
+++ js/firebase-config..js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDwQoMSbXokG53lk0BPfcenMX5NgPxDvU",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "tu-id",
+  appId: "tu-app-id"
+};
+
+export default firebaseConfig;
