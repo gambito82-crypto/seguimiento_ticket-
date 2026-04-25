8dbca9e8668c08048a760a7fa331f67f3f420485
gambito82-crypto <gambito82@gmail.com>
2026-04-24 21:32:08 -0400

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,12 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDDwQoMSbXokG53lk0BPfcenMX5NgPxDvU",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "tu-id",
-  appId: "tu-app-id"
+/**
+ * firebase-config.js — Firebase credentials (DO NOT commit to Git)
+ */
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBYPcIT_BDJtz6RU5fsO8qkpIB0eD1Jz64",
+  authDomain: "entrega-turnos-59960.firebaseapp.com",
+  databaseURL: "https://entrega-turnos-59960-default-rtdb.firebaseio.com",
+  projectId: "entrega-turnos-59960",
+  storageBucket: "entrega-turnos-59960.firebasestorage.app",
+  messagingSenderId: "145292534059",
+  appId: "1:145292534059:web:eb36147796524a8db1e56e"
 };
-
-export default firebaseConfig;
